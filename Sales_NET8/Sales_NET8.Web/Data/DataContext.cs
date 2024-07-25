3e86a8dbcd870d51328d74123794df3af02235a8
Tatiana Moraes <113339379+TatiM2801@users.noreply.github.com>
2024-07-25 21:00:40 +0100

    Countries controller

--- Sales_NET8/Sales_NET8.Web/Data/DataContext.cs
+++ Sales_NET8/Sales_NET8.Web/Data/DataContext.cs
@@ -7,9 +7,14 @@ namespace Sales_NET8.Web.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-
         }
 
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
     }
 }
